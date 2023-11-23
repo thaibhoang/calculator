5a490937fb81911b4abf59c82a83a2bb4f72f68c
thaibhoang <hoangthai.ftu53@gmail.com>
2023-11-23 19:42:31 +0700

    Create 4 simple math function and a operate function to call the math function depend on the operator variable

---
+++ script.js
@@ -0,0 +1,33 @@
+// basic math function
+function add(a, b) {
+    return a + b;
+} 
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if (b == 0) {
+        return 'cannot divide by 0';
+    } else {
+        return a / b;
+    }
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
